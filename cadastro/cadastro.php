145652655e8c0280973386131e2eeafd8c6ca0db
GabrielCadonaBasso <bassocadona.g@gmail.com>
2025-07-22 23:10:24 -0300

    Primeiro commit

---
+++ cadastro/cadastro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Tcc</title>
+</head>
+
+<body>
+    <div class="square">
+        <h1>Cadastro</h1>
+
+        <form action="POST" class="form-login">
+            <input type="text" name="" id="" placeholder="nome" class="input-text">
+            <input type="email" name="" id="" placeholder="e-mail" class="input-text">
+            <input type="password" name="" id="" placeholder="senha" class="input-text">
+
+            <div class="div-button">
+                <a href="../login/login.php" class="link">
+                    <button type="button" class="button-default">cadastrar</button>
+                </a>
+                <button type="button" class="button-default"><a href="../login/login.php" class="link">voltar</a></button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Tcc</title>
+</head>
+
+<body>
+    <div class="square">
+        <h1>Login</h1>
+
+        <form action="POST" class="form-login">
+            <input type="email" name="" id="" placeholder="e-mail" class="input-text">
+            <input type="password" name="" id="" placeholder="senha" class="input-text">
+
+            <div class="div-button">
+                <button type="button" class="button-default">logar</button>
+                <button type="button" class="button-default">cadastrar</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
