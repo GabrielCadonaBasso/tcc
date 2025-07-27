a66aeab4598473c104796a913f3122ef1ab5bb85
GabrielCadonaBasso <bassocadona.g@gmail.com>
2025-07-27 19:38:37 -0300

    ajustado

--- pagina_inicial/pagina_inicial.php
+++ pagina_inicial/pagina_inicial.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
+    <link rel="stylesheet" href="style.css">
 
     <?php
-        include_once '../functions.php';
+    include_once '../functions.php';
 
-        verificaSessao();
+    verificaSessao();
     ?>
 </head>
+
 <body>
-    <a href="../logout/logout.php">
-        <button name="button-sair">sair</button>
-    </a>
+    <nav>
+        <div class="hamburger" onclick="toggleMenu()">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+        <div class="menu" id="menu">
+            <a href="#home">Metas</a>
+            <a href="#home">Cadastrar Meta</a>
+        </div>
+
+        <a href="../logout/logout.php"><button class="logout">Sair</button></a>
+    </nav>
+
+    <script>
+        function toggleMenu() {
+            document.getElementById("menu").classList.toggle("active");
+        }
+    </script>
 </body>
+
 </html>
